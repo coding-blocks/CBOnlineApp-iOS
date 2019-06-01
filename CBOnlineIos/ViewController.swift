f1193dd3f6c2edc3f3a8f22578b80d099cd7a1d7
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-06-01 23:47:46 +0530

    add UIView extensions

--- CBOnlineIos/ViewController.swift
+++ CBOnlineIos/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import Alamofire
+import Japx
 
 class ViewController: UIViewController {
+    
+    let cardsDeckView = UIView()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
+        let cardView = CourseCard(frame: .zero)
+        cardView.nameLabel.text = "Android Development"
+
+        cardsDeckView.addSubview(cardView)
+        setupLayout()
+
+    }
+    
+    // MARK:- Fileprivate
+    
+    fileprivate func setupLayout() {
+        view.backgroundColor = .white
+        let overallStackView = UIStackView(arrangedSubviews: [cardsDeckView])
+        overallStackView.axis = .vertical
+        overallStackView.backgroundColor = .blue
+        view.addSubview(overallStackView)
+        overallStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
+        overallStackView.isLayoutMarginsRelativeArrangement = true
+        overallStackView.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
+        
+        overallStackView.bringSubviewToFront(cardsDeckView)
+        overallStackView.fillSuperview()
     }
 
 
---
+++ CBOnlineIos/Views/CourseCard.swift
@@ -0,0 +1,42 @@
+//
+//  CourseCard.swift
+//  CBOnlineIos
+//
+//  Created by Pulkit Aggarwal on 01/06/19.
+//  Copyright © 2019 Coding Blocks. All rights reserved.
+//
+
+import UIKit
+
+class CourseCard: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupLayout()
+    }
+    
+     let imageView = UIImageView(image: #imageLiteral(resourceName: "app_icon"))
+     let nameLabel = UILabel()
+
+    
+    fileprivate func setupLayout() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        backgroundColor = .blue
+        let padding: CGFloat = 16
+        let leftStackView = UIStackView(arrangedSubviews: [imageView,nameLabel])
+        addSubview(leftStackView)
+        leftStackView.axis = .vertical
+        leftStackView.distribution = .fillEqually
+        leftStackView.spacing  = padding
+    
+        
+    
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
