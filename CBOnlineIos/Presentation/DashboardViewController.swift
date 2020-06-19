e3ddcabaf84ed86f680d84beceac27a5e4c1c9d5
Pushpinder Pal Singh <pushpinder@tutamail.com>
2020-06-19 08:31:22 +0530

    Complete Nav Bar

--- CBOnlineIos/Presentation/DashboardViewController.swift
+++ CBOnlineIos/Presentation/DashboardViewController.swift
@@ -2,7 +2,7 @@
 //  DashboardViewController.swift
 //  CBOnlineIos
 //
-//  Created by Pushpinder Pal Singh on 13/06/20.
+//  Created by Pushpinder Pal Singh on 19/06/20.
 //  Copyright © 2020 Coding Blocks. All rights reserved.
 //
 
@@ -10,8 +10,22 @@ import UIKit
 
 class DashboardViewController: UIViewController {
 
+    @IBOutlet weak var navBarView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        navBarView.layer.cornerRadius = 20
+        // Do any additional setup after loading the view.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
