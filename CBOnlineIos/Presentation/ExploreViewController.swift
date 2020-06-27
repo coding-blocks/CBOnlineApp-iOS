32aef944dd26604ae136ff1eb6860a0a4188f3a7
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-06-27 11:48:21 +0530

    Add Top Button

--- CBOnlineIos/Presentation/ExploreViewController.swift
+++ CBOnlineIos/Presentation/ExploreViewController.swift
@@ -11,12 +11,24 @@ import UIKit
 class ExploreViewController: UIViewController {
     
     @IBOutlet weak var navBarView: UIView!
+    @IBOutlet weak var headerButton: UIView!
+    
+    let gradient = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavBar()
+        setHeaderButton()
+    }
+    func setNavBar(){
         navBarView.layer.cornerRadius = 20
         navBarView.clipsToBounds = true
         navBarView.layer.maskedCorners = [.layerMinXMaxYCorner,.layerMaxXMaxYCorner]
     }
-    
-    
+    func setHeaderButton(){
+        headerButton.layer.cornerRadius = 10
+        gradient.frame = headerButton.bounds
+        gradient.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        headerButton.layer.addSublayer(gradient)
+    }
 }
