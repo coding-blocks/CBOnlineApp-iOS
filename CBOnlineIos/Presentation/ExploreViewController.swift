42cfd098c50c291e72a06b0653bebb326c09e073
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-06-27 09:36:57 +0530

    Scroll View Added

--- CBOnlineIos/Presentation/ExploreViewController.swift
+++ CBOnlineIos/Presentation/ExploreViewController.swift
@@ -9,23 +9,14 @@
 import UIKit
 
 class ExploreViewController: UIViewController {
-
+    
     @IBOutlet weak var navBarView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         navBarView.layer.cornerRadius = 20
-        // Do any additional setup after loading the view.
+        navBarView.clipsToBounds = true
+        navBarView.layer.maskedCorners = [.layerMinXMaxYCorner,.layerMaxXMaxYCorner]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
