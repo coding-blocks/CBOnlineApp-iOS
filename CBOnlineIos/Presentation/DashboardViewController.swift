a0fceb2bb048050e59ae06a8509f762a9f5b7e46
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-07-19 22:10:00 +0530

    Add Your Performance

--- CBOnlineIos/Presentation/DashboardViewController.swift
+++ CBOnlineIos/Presentation/DashboardViewController.swift
@@ -11,21 +11,12 @@ import UIKit
 class DashboardViewController: UIViewController {
 
     @IBOutlet weak var navBarView: UIView!
+    @IBOutlet var performaceViews: [UIView]!
     override func viewDidLoad() {
         super.viewDidLoad()
         navBarView.layer.cornerRadius = 20
-        // Do any additional setup after loading the view.
+        for views in performaceViews{
+            views.layer.applySketchShadow(color: .gray, alpha: 0.3, x: 0, y: 0, blur: 0, spread: 1)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
