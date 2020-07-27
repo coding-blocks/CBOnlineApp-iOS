c7f55793fc7e0bf6d80cad290648f3d0ea9cdf95
Vaibhav Bisht <vaibhavbisht06@gmail.com>
2020-07-27 22:54:32 +0530

    Leading View check

--- CBOnlineIos/Presentation/Email.swift
+++ CBOnlineIos/Presentation/Email.swift
@@ -4,7 +4,7 @@ import UIKit
 import MaterialComponents.MaterialTextControls_FilledTextAreas
 import MaterialComponents.MaterialTextControls_FilledTextFields
 
-class Email: UIViewController {
+class Email: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailTextField: MDCFilledTextField!
     @IBOutlet weak var passwordTextField: MDCFilledTextField!
     @IBOutlet weak var toggleSecureButton: UIButton!
@@ -39,23 +39,25 @@ class Email: UIViewController {
         passwordTextField.setTextColor(UIColor.black, for: .normal)
         emailTextField.tintColor = UIColor.orange
         passwordTextField.tintColor = UIColor.orange
-        iconClick = true
-        
+//        passwordTextField.trailingView = UIView(frame: CGRect(x: 0, y: 0, width: 27, height: 22))
+//        passwordTextField.trailingView?.backgroundColor = UIColor(patternImage: UIImage(systemName: "eye.fill"))
+          
         if #available(iOS 13.0, *) {
-            toggleSecureButton.setImage(UIImage(systemName: "eye.fill"), for: .normal)
+            passwordTextField.trailingView = UIImageView(image: UIImage(systemName: "eye.fill"))
+            passwordTextField.leadingViewMode = .always
         } else {
             // Fallback on earlier versions
-            return
         }
-        
-        
+        iconClick = true
     }
     // For secure Text
     @IBAction func showSecureButton(_ sender: UIButton) {
         if(iconClick == true){
             passwordTextField.isSecureTextEntry = false
             if #available(iOS 13.0, *) {
-                toggleSecureButton.setImage(UIImage(systemName: "eye.slash.fill"), for: .normal)
+//                toggleSecureButton.setImage(UIImage(systemName: "eye.slash.fill"), for: .normal)
+                passwordTextField.trailingView = UIImageView(image: UIImage(systemName: "eye.slash.fill"))
+                           passwordTextField.leadingViewMode = .always
             } else {
                 // Fallback on earlier versions
                 return
@@ -65,7 +67,9 @@ class Email: UIViewController {
         else{
             passwordTextField.isSecureTextEntry = true
             if #available(iOS 13.0, *) {
-                toggleSecureButton.setImage(UIImage(systemName: "eye.fill"), for: .normal)
+//                toggleSecureButton.setImage(UIImage(systemName: "eye.fill"), for: .normal)
+                passwordTextField.trailingView = UIImageView(image: UIImage(systemName: "eye.fill"))
+                           passwordTextField.leadingViewMode = .always
             } else {
                 // Fallback on earlier versions
                 return
@@ -77,19 +81,21 @@ class Email: UIViewController {
         userEmail = emailTextField.text ?? ""
         userPassword = passwordTextField.text ?? ""
         if userEmail.isEmpty{
-            emailFieldIsEmplty()
+//            emailFieldIsEmplty()
+            errorCalling(errorMessage: "Please enter email address")
         }
         else{
             if userPassword.isEmpty{
-                passwrdFiledIsEmplty()
+//                passwrdFiledIsEmplty()
+                errorCalling(errorMessage: "Please enter password")
             }
             else{
                 if isValidEmail(emailID: userEmail) == false{
-                    invalidEmail()
+//                    invalidEmail()
+                    errorCalling(errorMessage: "Please enter valid email address")
                 }
                 else{
-                    // As of now printing but in future userEmail and userPassword will be used for interaction
-                    print(userEmail + " " + userPassword )
+                    // will use userEmail and userPasword for internaction
                 }
             }
             
@@ -97,23 +103,8 @@ class Email: UIViewController {
     }
     
     // Alert controller function
-    func emailFieldIsEmplty(){
-        let alertController = UIAlertController(title: "Warning", message: "Please enter email address", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK",style: .cancel, handler:nil)
-        alertController.addAction(okAction)
-        self.present(alertController, animated: true)
-    }
-    
-    // Alert controller function
-    func passwrdFiledIsEmplty(){
-        let alertController = UIAlertController(title: "Warning", message: "Please enter password", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK",style: .cancel, handler:nil)
-        alertController.addAction(okAction)
-        self.present(alertController, animated: true)
-    }
-    
-    func invalidEmail(){
-        let alertController = UIAlertController(title: "Warning", message: "Please enter valid email address", preferredStyle: .alert)
+    func errorCalling (errorMessage :String) {
+        let alertController = UIAlertController(title: "Warning", message: errorMessage , preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK",style: .cancel, handler:nil)
         alertController.addAction(okAction)
         self.present(alertController, animated: true)
---
+++ Pods/MaterialComponents/components/private/TextControlsPrivate/src/FilledStyle/MDCTextControlVerticalPositioningReferenceFilled.m
@@ -0,0 +1,67 @@
+// Copyright 2019-present the Material Components for iOS authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "MDCTextControlVerticalPositioningReferenceFilled.h"
+#import "MDCTextControlVerticalPositioningReference.h"
+
+/**
+ For slightly more context on what this class is doing look at
+ MDCTextControlVerticalPositioningReferenceBase. It's very similar and has some comments. Maybe at
+ some point all the positioning references should be refactored to share a superclass, because
+ there's currently a lot of duplicated code among the three of them.
+*/
+@interface MDCTextControlVerticalPositioningReferenceFilled ()
+@end
+
+@implementation MDCTextControlVerticalPositioningReferenceFilled
+
+@synthesize paddingBetweenContainerTopAndNormalLabel = _paddingBetweenContainerTopAndNormalLabel;
+
+- (instancetype)initWithFloatingFontLineHeight:(CGFloat)floatingLabelHeight
+                          normalFontLineHeight:(CGFloat)normalFontLineHeight
+                                 textRowHeight:(CGFloat)textRowHeight
+                              numberOfTextRows:(CGFloat)numberOfTextRows
+                                       density:(CGFloat)density
+                      preferredContainerHeight:(CGFloat)preferredContainerHeight {
+  self = [super initWithFloatingFontLineHeight:floatingLabelHeight
+                          normalFontLineHeight:normalFontLineHeight
+                                 textRowHeight:textRowHeight
+                              numberOfTextRows:numberOfTextRows
+                                       density:density
+                      preferredContainerHeight:preferredContainerHeight];
+  if (self) {
+    BOOL isMultiline = numberOfTextRows > 1 || numberOfTextRows == 0;
+    CGFloat halfOfNormalFontLineHeight = (CGFloat)0.5 * normalFontLineHeight;
+    if (isMultiline) {
+      CGFloat heightWithOneRow = MDCTextControlCalculateContainerHeightWithFloatingLabelHeight(
+          floatingLabelHeight, textRowHeight, 1, self.paddingBetweenContainerTopAndFloatingLabel,
+          self.paddingBetweenFloatingLabelAndEditingText,
+          self.paddingBetweenEditingTextAndContainerBottom);
+      CGFloat halfOfHeightWithOneRow = (CGFloat)0.5 * heightWithOneRow;
+      _paddingBetweenContainerTopAndNormalLabel =
+          halfOfHeightWithOneRow - halfOfNormalFontLineHeight;
+    } else {
+      CGFloat halfOfContainerHeight = (CGFloat)0.5 * self.containerHeightWithFloatingLabel;
+      _paddingBetweenContainerTopAndNormalLabel =
+          halfOfContainerHeight - halfOfNormalFontLineHeight;
+    }
+  }
+  return self;
+}
+
+- (CGFloat)paddingBetweenContainerTopAndNormalLabel {
+  return _paddingBetweenContainerTopAndNormalLabel;
+}
+
+@end
