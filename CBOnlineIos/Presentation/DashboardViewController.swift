bc428223fa332fe4bb3facd8c43b7a34dd406fe1
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-07-30 17:20:21 +0530

    Add Curve

---
+++ CBOnlineIos/Presentation/DashboardViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DashboardViewController.swift
+//  CBOnlineIos
+//
+//  Created by Pushpinder Pal Singh on 30/07/20.
+//  Copyright © 2020 Coding Blocks. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    @IBOutlet weak var navigationView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationView.layer.cornerRadius = 20
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
