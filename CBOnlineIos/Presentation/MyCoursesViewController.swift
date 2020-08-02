a460c42f7dede6165b21fe2b04053484068ae818
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-08-02 11:21:48 +0530

    My Courses View
    Signed-off-by: Pushpinder Pal Singh <pushpinderpal19@gmail.com>

--- CBOnlineIos/Presentation/MyCoursesViewController.swift
+++ CBOnlineIos/Presentation/MyCoursesViewController.swift
@@ -1,83 +1,121 @@
-//
-//  MyCoursesViewController.swift
-//  CBOnlineIos
-//
-//  Created by Pushpinder Pal Singh on 23/06/20.
-//  Copyright © 2020 Coding Blocks. All rights reserved.
-//
-
 import UIKit
 
-class MyCoursesViewController: UIViewController {
-
+class MyCoursesViewController: UIViewController, UITableViewDelegate {
+    
     @IBOutlet weak var recentlyAdded: UIButton!
     @IBOutlet weak var MyCourseTableView: UITableView!
+    
+    var transparentView = UIView()
+    var tableView = UITableView()
+    let height: CGFloat = UIScreen.main.bounds.height / 2
+    
+    var settingArray = ["Recently Accessed","Expired Courses","Free Trials","All Courses","Premium","Active"]
+    var imageArray = ["play.fill","exclamationmark.triangle","lock","video","star","play"]
+    
     override func viewDidLoad() {
-           super.viewDidLoad()
-           self.view = view
+        super.viewDidLoad()
+        self.view = view
         MyCourseTableView.dataSource = self
         MyCourseTableView.register(UINib(nibName: "MyCourseTableViewCell", bundle: nil), forCellReuseIdentifier: "MyCourseTableViewCell")
-           setUPCardView()
-           setUpAnimationbutton()
-       }
-       let cardView = UIView()
-       
-       let recentlyAccessed  = UIButton()
-       let expiredCourses = UIButton()
-       let freeTrials = UIButton()
-       let allCourses = UIButton()
-       let premium = UIButton()
-       let active = UIButton()
-       var cardViewBottom : NSLayoutConstraint!
-       
-       func setUPCardView(){
-           view.addSubview(cardView)
-           cardView.backgroundColor = .black
-           cardView.layer.cornerRadius = 12
-           setupCardViewConstrains()
-           setUpRecentlyButton()
-       }
-       
-       func setUpRecentlyButton() {
-           cardView.addSubview(recentlyAccessed)
-           recentlyAccessed.setTitle("Recently Added", for: .normal)
-           
-       }
-       
-       func setUpAnimationbutton(){
-           recentlyAdded.addTarget(self, action: #selector(animate), for: .touchUpInside)
-       }
-       @objc func animate(){
-           cardView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10).isActive = true
-           UIView.animate(withDuration: 0.4) {
-               self.view.layoutIfNeeded()
-           }
-       }
-       
-       
-       func setupCardViewConstrains(){
-           cardView.translatesAutoresizingMaskIntoConstraints = false
-           cardView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
-           cardView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
-           cardView.heightAnchor.constraint(equalToConstant: 360).isActive = true
-           cardViewBottom = cardView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 500)
-           cardViewBottom.isActive = true
-       }
-       
+        
+        tableView.isScrollEnabled = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CustomBottomSheetTableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+    }
+    
+    @IBAction func recentlyPressed(_ sender: UIButton) {
+        let window = UIApplication.shared.keyWindow
+        transparentView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+        transparentView.frame = self.view.frame
+        window?.addSubview(transparentView)
+        
+        let screenSize = UIScreen.main.bounds.size
+        tableView.frame = CGRect(x: 0, y: screenSize.height, width: screenSize.width, height: height)
+        tableView.layer.cornerRadius = 20
+        window?.addSubview(tableView)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(onClickTransparentView))
+        transparentView.addGestureRecognizer(tapGesture)
+        
+        transparentView.alpha = 0
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+            self.transparentView.alpha = 0.5
+            self.tableView.frame = CGRect(x: 0, y: screenSize.height - self.height, width: screenSize.width, height: self.height)
+        }, completion: nil)
+    }
+    
+    @IBAction func pleasework(_ sender: Any) {
+        let window = UIApplication.shared.keyWindow
+        transparentView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+        transparentView.frame = self.view.frame
+        window?.addSubview(transparentView)
+        
+        let screenSize = UIScreen.main.bounds.size
+        tableView.frame = CGRect(x: 0, y: screenSize.height, width: screenSize.width, height: height)
+        tableView.layer.cornerRadius = 20
+        window?.addSubview(tableView)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(onClickTransparentView))
+        transparentView.addGestureRecognizer(tapGesture)
+        
+        transparentView.alpha = 0
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+            self.transparentView.alpha = 0.5
+            self.tableView.frame = CGRect(x: 0, y: screenSize.height - self.height, width: screenSize.width, height: self.height)
+        }, completion: nil)
+    }
+    
+    
+    @objc func onClickTransparentView() {
+        let screenSize = UIScreen.main.bounds.size
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+            self.transparentView.alpha = 0
+            self.tableView.frame = CGRect(x: 0, y: screenSize.height, width: screenSize.width, height: self.height)
+        }, completion: nil)
+    }
+    
 }
+
+
+
+
 
 extension MyCoursesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2;
+        if tableView == MyCourseTableView {
+            return 2
+        }
+        return settingArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCourseTableViewCell", for: indexPath) as! MyCourseTableViewCell
-        cell.headerLabel.text = "Competitive Programming"
-        cell.detailLabel.text = "Mentor:Apaar Kamal and 1 other"
-        cell.progressView.progress = 50
-        cell.courseImage.image = UIImage(named: "courseImage")
-        return cell
+        if tableView == MyCourseTableView {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyCourseTableViewCell", for: indexPath) as! MyCourseTableViewCell
+            cell.headerLabel.text = "Competitive Programming"
+            cell.detailLabel.text = "Mentor:Apaar Kamal and 1 other"
+            cell.progressView.progress = 50
+            cell.courseImage.image = UIImage(named: "courseImage")
+            return cell
+        }
+        else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomBottomSheetTableViewCell
+            cell.lbl.font = cell.lbl.font.withSize(20)
+            cell.lbl.text = settingArray[indexPath.row]
+            if indexPath.row == 0 {
+                cell.lbl.textColor = UIColor(red: 0.89, green: 0.482, blue: 0.34, alpha: 1)
+            }
+            cell.settingImage.image = UIImage(systemName: imageArray[indexPath.row])
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75
     }
     
     
