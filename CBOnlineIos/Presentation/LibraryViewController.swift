f1cd66be0510ac4cbaa648d6cc3cd0cf40ecd63a
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-08-02 12:33:09 +0530

    Add Curve
    Signed-off-by: Pushpinder Pal Singh <pushpinderpal19@gmail.com>

---
+++ CBOnlineIos/Presentation/LibraryViewController.swift
@@ -0,0 +1,19 @@
+
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+
+    @IBOutlet weak var NavigationBar: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+                NavigationBar.layer.cornerRadius = 20
+        NavigationBar.layer.borderWidth = 0.2
+        
+        NavigationBar.layer.maskedCorners = [.layerMinXMaxYCorner,.layerMaxXMaxYCorner]
+    }
+
+
+}
